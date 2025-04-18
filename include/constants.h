93b281613b2d8b7fae92a8babe9b5608a59de7f9
Arnav Jain <arnavj214@gmail.com>
2025-04-18 15:49:06 -0700

    change vel limit

--- include/constants.h
+++ include/constants.h
@@ -49,7 +49,7 @@ constexpr u32 R_WHEEL_GEAR_SAMPLE_WINDOW = 1;
  
 // ODrive
 constexpr u8 ODRIVE_NODE_ID = 0x3;                            
-constexpr float ODRIVE_VEL_LIMIT = 40.0;        // rot / s    //TODO: CHange this to reasonable value
+constexpr float ODRIVE_VEL_LIMIT = 80.0;        // rot / s    //TODO: CHange this to reasonable value
 constexpr float ODRIVE_CURRENT_SOFT_MAX = 20.0; // A          // TODO: Check if this is the right value 
 
 // Driver Interface
