cf8d8baf47b2f25c5b6c64e845d9f378e5645531
Grant Pauker <grantpauker@gmail.com>
2025-04-19 20:50:28 -0700

    Add position command info to logs

--- src/main.cpp
+++ src/main.cpp
@@ -388,8 +388,8 @@ void control_function() {
   last_engine_rpm_error = filtered_engine_rpm_error;
 
 
-  float actuator_offset = ((wheel_mph - WHEEL_REF_BREAKPOINT_LOW_MPH) * ACTUATOR_OFFSET_SLOPE) + ACTUATOR_OFFSET_LOW;
-  actuator_offset = CLAMP(actuator_offset, ACTUATOR_OFFSET_HIGH, ACTUATOR_OFFSET_LOW);
+  control_state.actuator_offset = ((wheel_mph - WHEEL_REF_BREAKPOINT_LOW_MPH) * ACTUATOR_OFFSET_SLOPE) + ACTUATOR_OFFSET_LOW;
+  control_state.actuator_offset = CLAMP(control_state.actuator_offset, ACTUATOR_OFFSET_HIGH, ACTUATOR_OFFSET_LOW);
 
   control_state.engine_rpm_error_integral += control_state.engine_rpm_error * dt_s;
 
@@ -402,8 +402,9 @@ void control_function() {
   }
 
   // **** NOTE **** Actuator offset is negative because of polarity of ODrive (shifting in is negative, shifting out is positive)
-  control_state.position_command = actuator_offset + control_state.engine_rpm_error * ACTUATOR_KP + control_state.engine_rpm_error_integral * ACTUATOR_KI;
-  float pid_value = control_state.engine_rpm_error * ACTUATOR_KP + control_state.engine_rpm_error_integral * ACTUATOR_KI;
+  //
+  control_state.pi_position_command = control_state.engine_rpm_error * ACTUATOR_KP + control_state.engine_rpm_error_integral * ACTUATOR_KI;
+  control_state.position_command = control_state.actuator_offset  + control_state.pi_position_command;
   control_state.position_command = CLAMP(control_state.position_command, ACTUATOR_MIN_POS, ACTUATOR_MAX_POS);
   /*
   // to not interfere with starting the car 
